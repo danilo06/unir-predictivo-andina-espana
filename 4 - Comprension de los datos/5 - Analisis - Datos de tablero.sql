38c49eb0bb27c0bf1e18dbb16a99cce3a20a8753
Danilo Beleño <b.daniloandres@hotmail.com>
2024-12-25 15:48:03 -0500

    ajuste de descripciones

--- 4 - Comprension de los datos/5 - Analisis - Datos de tablero.sql
+++ 4 - Comprension de los datos/5 - Analisis - Datos de tablero.sql
@@ -19,10 +19,8 @@ with comex as (
         SUBSTR(cod_taric, 1, 8) AS cod_taric_4,
         cod_taric AS cod_taric_5,
         descripcion_taric as taric_desc_5
-    FROM
-        `unir-predictiv0-andina-espana.datacomex.comex_comunidad_andina`
-    WHERE
-        comex_nivel_taric = '5'
+    FROM `unir-predictiv0-andina-espana.datacomex.comex_comunidad_andina`
+    WHERE comex_nivel_taric = '5'
 )
 select
     comex.fecha,
@@ -34,18 +32,22 @@ select
     comex.dolares,
     comex.kilogramos,
     comex.cod_taric_1,
-    desc_lvl1.descripcion_taric as taric_desc_1,
+    REGEXP_REPLACE(RTRIM(TRIM(REGEXP_REPLACE(desc_lvl1.descripcion_taric, r'^[_\s]+', '')), ':'), r'::+$', '') AS taric_desc_1,
     comex.cod_taric_2,
-    desc_lvl2.descripcion_taric as taric_desc_2,
+    REGEXP_REPLACE(RTRIM(TRIM(REGEXP_REPLACE(desc_lvl2.descripcion_taric, r'^[_\s]+', '')), ':'), r'::+$', '') AS taric_desc_2,
     comex.cod_taric_3,
-    desc_lvl3.descripcion_taric as taric_desc_3,
+    REGEXP_REPLACE(RTRIM(TRIM(REGEXP_REPLACE(desc_lvl3.descripcion_taric, r'^[_\s]+', '')), ':'), r'::+$', '') AS taric_desc_3,
     comex.cod_taric_4,
-    desc_lvl4.descripcion_taric as taric_desc_4,
+    REGEXP_REPLACE(RTRIM(TRIM(REGEXP_REPLACE(desc_lvl4.descripcion_taric, r'^[_\s]+', '')), ':'), r'::+$', '') AS taric_desc_4,
     comex.cod_taric_5,
-    comex.taric_desc_5
+    REGEXP_REPLACE(RTRIM(TRIM(REGEXP_REPLACE(comex.taric_desc_5, r'^[_\s]+', '')), ':'), r'::+$', '') AS taric_desc_5
 from
     comex
-    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` as desc_lvl1 ON comex.cod_taric_1 = desc_lvl1.cod_taric
-    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` as desc_lvl2 ON comex.cod_taric_2 = desc_lvl2.cod_taric
-    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` as desc_lvl3 ON comex.cod_taric_3 = desc_lvl3.cod_taric
-    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` as desc_lvl4 ON comex.cod_taric_4 = desc_lvl4.cod_taric
+    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` AS desc_lvl1 
+        ON comex.cod_taric_1 = desc_lvl1.cod_taric
+    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` AS desc_lvl2 
+        ON comex.cod_taric_2 = desc_lvl2.cod_taric
+    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` AS desc_lvl3 
+        ON comex.cod_taric_3 = desc_lvl3.cod_taric
+    LEFT JOIN `unir-predictiv0-andina-espana.datacomex.clasificacion_taric` AS desc_lvl4 
+        ON comex.cod_taric_4 = desc_lvl4.cod_taric
